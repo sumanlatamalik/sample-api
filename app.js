b15774d48fc885eb89bb973a79cf934a25b8ea72
Suman Lata Malik <sumanlata.malik@mi.medical.canon>
2025-08-24 18:33:47 -0400

    Initial commit of sample API

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from my sample API!');
+});
+
+app.listen(port, () => {
+    console.log(`API running on port ${port}`);
+});
