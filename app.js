17a9faf2e7422e0951c7fa17d20dcc6acf0afc65
Suman Lata Malik <sumanlata.malik@mi.medical.canon>
2025-08-24 20:33:02 -0400

    Update API message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from my sample API!');
+    res.send('Hello from my updated API!');
 });
 
 app.listen(port, () => {
